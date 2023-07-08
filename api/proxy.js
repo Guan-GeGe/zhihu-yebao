b690178c3ab5330cc2e9cada5638feed49995372
lwh <2352416774@qq.com>
2023-07-08 22:28:37 +0800

    12312

--- api/proxy.js
+++ api/proxy.js
@@ -2,14 +2,14 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "";
+  let target = "https://news-at.zhihu.com/api/4";
 
   // 代理目标地址
   // xxxxx 替换为你跨域请求的服务器 如： http://baidu.com
-  if (req.url.startsWith("/api")) {
-    // 这里填目标地址
-    target = "https://news-at.zhihu.com/api/4";
-  }
+  // if (req.url.startsWith("/api")) {
+  //   // 这里填目标地址
+  //   target = "";
+  // }
   // 创建代理对象并转发请求
   createProxyMiddleware({
     target,
@@ -17,7 +17,7 @@ module.exports = (req, res) => {
     pathRewrite: {
       // 通过路径重写，去除请求路径中的 `/api`
       // 例如 /api/user/login 将被转发到 http://target/user/login
-      "^/api/(.*)": "",
+      "/api": "",
     },
   })(req, res);
   console.log(req, res, "req,res");
--- src/service/home/home.js
+++ src/service/home/home.js
@@ -2,7 +2,7 @@ import WHRequest from "..";
 // 获取今日信息 轮播图信息
 export const queryNewsLatest = () => {
   return WHRequest.get({
-    url: "/news_latest",
+    url: "/api/news_latest",
   });
 };
 
